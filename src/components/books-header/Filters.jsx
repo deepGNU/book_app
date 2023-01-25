9336c6063b635a8250cd39d16f43afece52677ad
IsaacPride <thehumbletheorist@gmail.com>
2023-01-25 10:55:10 +0200

    filter vars to mode & displaying when on in books

--- src/components/books-header/Filters.jsx
+++ src/components/books-header/Filters.jsx
@@ -1,32 +1,49 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { filterBooks } from '../../features/books-slice';
+import { setFilterDateAfter, setFilterDateBefore, setFilterMinRating } from '../../features/mode-slice';
 
 const Filters = () => {
     const dispatch = useDispatch();
     const books = useSelector((s) => s.book.books);
     const showFilters = useSelector((s) => s.mode.showFilters);
-    const [datePrior, setDatePrior] = useState();
-    const [dateAfter, setDateAfter] = useState();
-    const [filterRating, setFilterRating] = useState();
+    const datePrior = useSelector((s) => s.mode.filterDateBefore);
+    const dateAfter = useSelector((s) => s.mode.filterDateAfter);
+    const filterRating = useSelector((s) => s.mode.filterMinRating);
+    // const [datePrior, setDatePrior] = useState(prev => prev);
+    // const [dateAfter, setDateAfter] = useState(prev => prev);
+    // const [filterRating, setFilterRating] = useState(prev => prev);
+    // console.log(dateAfter, datePrior, filterRating)
 
     useEffect(() => {
         const booksFiltered = books.filter((b) => {
-            return (!dateAfter || Date.parse(b.volumeInfo.publishedDate) > dateAfter) &&
-                (!datePrior || Date.parse(b.volumeInfo.publishedDate) < datePrior) &&
-                (!filterRating || b.volumeInfo.averageRating >= filterRating);
+            return (!dateAfter || Date.parse(b.volumeInfo?.publishedDate) > dateAfter) &&
+                (!datePrior || Date.parse(b.volumeInfo?.publishedDate) < datePrior) &&
+                (!filterRating || b.volumeInfo?.averageRating >= filterRating);
         });
 
         dispatch(filterBooks(booksFiltered));
     }, [dispatch, books, dateAfter, datePrior, filterRating]);
+
+    const handleRatingChange = (e) => {
+        dispatch(setFilterMinRating(e.target.value));
+    };
+
+    const handleDateAfterChange = (e) => {
+        dispatch(setFilterDateAfter(Date.parse(e.target.value)));
+    };
+
+    const handleDatePriorChange = (e) => {
+        dispatch(setFilterDateBefore(Date.parse(e.target.value)));
+    };
 
     return (
         <div className={`${showFilters ? 'filters' : 'hide'}`}>
             <div className='filter-div'>
                 <label htmlFor="rating">Minimal Rating:&nbsp;</label>
                 <select
-                    name="" id="rating" value={filterRating}
-                    onChange={(e) => setFilterRating(e.target.value)}
+                    name="" id="rating" value={filterRating || ''}
+                    onChange={handleRatingChange}
                 >
                     <option value="">No Filter</option>
                     {[...Array(5).keys()].map((x) => <option key={x} value={x + 1}>{x + 1}</option>)}
@@ -37,8 +54,8 @@ const Filters = () => {
                 <label htmlFor="published-after">Published After:&nbsp;</label>
                 <select
                     name="" id="published-after"
-                    value={new Date(dateAfter).getFullYear().toString()}
-                    onChange={(e) => setDateAfter(Date.parse(e.target.value))}
+                    value={dateAfter ? new Date(dateAfter).getFullYear().toString() : ''}
+                    onChange={handleDateAfterChange}
                 >
                     <option value="">No Filter</option>
                     {Array.from({ length: 500 }, (_, i) => new Date().getFullYear() - i).map((y) => <option key={y} value={y}>{y}</option>)}
@@ -49,8 +66,8 @@ const Filters = () => {
                 <label htmlFor="published-prior">Published Before:&nbsp;</label>
                 <select
                     name="" id="published-prior"
-                    value={new Date(datePrior).getFullYear().toString()}
-                    onChange={(e) => setDatePrior(Date.parse(e.target.value))}
+                    value={datePrior ? new Date(datePrior).getFullYear().toString() : ''}
+                    onChange={handleDatePriorChange}
                 >
                     <option value="">No Filter</option>
                     {Array.from({ length: 500 }, (_, i) => new Date().getFullYear() - i).map((y) => <option key={y} value={y}>{y}</option>)}
