daa9140a33f8ad7ddd57c0215e7a718e11456c04
IsaacPride <thehumbletheorist@gmail.com>
2023-01-28 16:32:33 +0200

    if in editing mode, leaves

--- src/components/header-buttons/SelectBtn.jsx
+++ src/components/header-buttons/SelectBtn.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { leaveEditMode } from '../../features/mode-slice';
 
 const SelectBtn = ({ onClick, selecting, isMenuCollapsed = false }) => {
+    const editing = useSelector((s) => s.mode.editing);
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        if (editing) dispatch(leaveEditMode());
+        onClick();
+    };
+
     return (
         <button
             className={`btn btn-top${(isMenuCollapsed && !selecting) ?
                 ' hide-if-narrow' : '' + (selecting ? ' btn-active' : '')}`}
             title={`${selecting ? 'Cancel Selection' : 'Select Items'}`}
-            onClick={onClick}
+            onClick={handleClick}
         >
             {selecting ? 'Cancel' : 'Select'}
         </button>
