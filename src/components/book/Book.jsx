9faa60b72d632608015535b5a9ab34b38c01fe04
IsaacPride <thehumbletheorist@gmail.com>
2023-01-29 15:11:36 +0200

    final clean up

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,3 +1,4 @@
+import '../book-buttons/BookButtons.css';
 import './Book.css';
 import { useNavigate, useLocation } from 'react-router-dom';
 import BookButtons from '../book-buttons/BookButtons';
@@ -8,25 +9,29 @@ const Book = ({ book, selecting, toggleSelected }) => {
   const location = useLocation();
   const path = location.pathname === '/' ? '/books' : location.pathname;
 
+  const bookClasses = `book-card${(selecting && !book.isSelected ? ' faded' : '') +
+    (book.isSelected ? ' book-selected' : '') +
+    (!selecting ? ' not-selecting' : '')}`;
+
+  const handleClick = () => selecting ? toggleSelected(book.id)
+    : navigate(`${path}/${book.id}`);
+
   return (
     <div
-      className={`book-card${(selecting && !book?.isSelected ? ' faded' : '') +
-        (book?.isSelected ? ' book-selected' : '') +
-        (!selecting ? ' not-selecting' : '')}`}
+      className={bookClasses}
       title={selecting ? 'Select Item' : 'See Details'}
-      onClick={() => selecting ? toggleSelected(book.id)
-        : navigate(`${path}/${book?.id}`)}
+      onClick={handleClick}
     >
       <div className='top-book-card'>
-        <img className='img-book-card' src={book?.volumeInfo?.imageLinks?.thumbnail} alt='Cover' />
+        <img className='img-book-card' src={book.volumeInfo?.imageLinks?.thumbnail} alt='Cover' />
         <hr />
         <div className='text-book-card'>
-          <h5 className='book-title'>{book?.volumeInfo?.title}</h5>
-          <h6 className='book-title'>{book?.volumeInfo?.subtitle}</h6>
+          <h5 className='book-title'>{book.volumeInfo?.title}</h5>
+          <h6 className='book-title'>{book.volumeInfo?.subtitle}</h6>
           <p>
-            {book?.volumeInfo?.authors}
+            {book.volumeInfo?.authors}
             &nbsp;&#9679;&nbsp;
-            {book?.volumeInfo?.publishedDate}
+            {book.volumeInfo?.publishedDate}
           </p>
         </div>
         <hr />
