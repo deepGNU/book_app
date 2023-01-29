232d4049ccdb98251e50daa78af08403f7269752
IsaacPride <thehumbletheorist@gmail.com>
2023-01-29 17:24:57 +0200

    final clean up

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import TopNav from "./components/nav/TopNav";
-import useBooks from "./hooks/useBooks";
-import About from "./pages/about/About";
-import BookDetails from "./pages/book-details/BookDetails";
-import Books from "./pages/books/Books";
-import Favorites from "./pages/favorites/Favorites";
-import NotFound404 from "./pages/not-found-404/NotFound404";
-import BookEditWindow from "./components/book-add-edit/BookEditForm";
 import { useSelector } from "react-redux";
 import { ToastContainer } from 'react-toastify';
+import useBooks from "./hooks/useBooks";
+import useScrollToTop from "./hooks/useScrollToTop";
+import TopNav from "./components/nav/TopNav";
+import BookEditWindow from "./components/book-add-edit/BookEditForm";
 import BookAddWindow from "./components/book-add-edit/BookAddForm";
-import useScrollToTop from "./hooks/useScrollToTopOfNewPage";
+import Books from "./pages/books/Books";
+import Favorites from "./pages/favorites/Favorites";
+import BookDetails from "./pages/book-details/BookDetails";
+import About from "./pages/about/About";
+import NotFound404 from "./pages/not-found-404/NotFound404";
 
 function App() {
   useBooks();
   useScrollToTop();
   const editing = useSelector((s) => s.mode.editing);
   const adding = useSelector((s) => s.mode.adding);
-  const loading = useSelector((s) => s.book.loading);
 
   return (
-    <div className={`App${loading ? ' overflow-hidden' : ''}`}>
+    <div className='App'>
       <ToastContainer />
       <TopNav />
       {editing && <BookEditWindow />}
@@ -37,6 +36,6 @@ function App() {
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
